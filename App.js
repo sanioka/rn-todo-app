5eade850a90e7c47c00884f1bb698301445114e2
AIvanov <isanioka@gmail.com>
2021-05-01 21:26:52 +0300

    navbar, input and add button

--- App.js
+++ App.js
@@ -2,20 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Navbar } from "./src/Navbar";
+import { AddTodo } from "./src/AddToto";
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title="Todo App"/>
+      <View style={styles.container}>
+        <AddTodo />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
   },
 });
