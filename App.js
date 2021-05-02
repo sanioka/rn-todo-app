451d5a212e946dd2569e47adc3d7c352406cedd3
AIvanov <isanioka@gmail.com>
2021-05-02 14:28:56 +0300

    image if no any todos

--- App.js
+++ App.js
@@ -8,11 +8,11 @@ import { TodoScreen } from "./src/screens/TodoScreen";
 
 export default function App() {
   const [todos, setTodos] = useState([
-    {id: '1', title: 'todo1'},
-    {id: '2', title: 'todo2'},
-    {id: '3', title: 'todo3'},
-    {id: '4', title: 'todo4'},
-    {id: '5', title: 'todo5'},
+    // {id: '1', title: 'todo1'},
+    // {id: '2', title: 'todo2'},
+    // {id: '3', title: 'todo3'},
+    // {id: '4', title: 'todo4'},
+    // {id: '5', title: 'todo5'},
   ]);
   const [todoId, setTodoID] = useState(null);
 
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,23 +1,41 @@
 import React from 'react';
 
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import { Todo } from "../components/Todo";
 import { AddTodo } from "../components/AddToto";
 
 export const MainScreen = ({todos, addTodo, removeTodo, openTodo}) => {
+  let content = (
+    <FlatList
+      data={todos}
+      renderItem={({item}) => <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>}
+      keyExtractor={item => item.id}
+    />
+  )
+
+  if (todos.length === 0) {
+    content = <View style={styles.imageWrap}>
+      <Image style={styles.image} source={require('../../assets/no-items.png')}/>
+    </View>
+  }
   return (
     <View>
       <AddTodo onSubmit={addTodo}/>
-
-      <FlatList
-        data={todos}
-        renderItem={({item}) => <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>}
-        keyExtractor={item => item.id}
-      />
+      {content}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  imageWrap: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    height: 300,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  }
 })
